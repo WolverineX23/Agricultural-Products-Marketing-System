4644494abcfa89a56b1e3f6ffd90adaf014ca147
547036318 <547036318@qq.com>
2020-06-07 21:26:01 +0800

    form4功能完善

---
+++ Crops.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 农产品物流管理系统
+{
+    class Crops
+    {
+        public string cno, cname;
+        public int freshness;
+        public Crops() { }
+        public Crops(string cno,string cname,int freshness)
+        {
+            this.cno = cno;
+            this.cname = cname;
+            this.freshness = freshness;
+        }
+    }
+}
--- Form4.Designer.cs
+++ Form4.Designer.cs
@@ -40,7 +40,7 @@
             this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button1.FlatAppearance.BorderColor = System.Drawing.SystemColors.Control;
             this.button1.Font = new System.Drawing.Font("楷体", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button1.Location = new System.Drawing.Point(77, 123);
+            this.button1.Location = new System.Drawing.Point(77, 171);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(230, 80);
             this.button1.TabIndex = 0;
@@ -52,18 +52,19 @@
             // 
             this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button2.Font = new System.Drawing.Font("楷体", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button2.Location = new System.Drawing.Point(77, 251);
+            this.button2.Location = new System.Drawing.Point(77, 367);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(230, 80);
             this.button2.TabIndex = 1;
             this.button2.Text = "仓库";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
             this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button3.Font = new System.Drawing.Font("楷体", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button3.Location = new System.Drawing.Point(77, 373);
+            this.button3.Location = new System.Drawing.Point(77, 570);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(230, 80);
             this.button3.TabIndex = 2;
@@ -72,29 +73,29 @@
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(330, 123);
+            this.richTextBox1.Font = new System.Drawing.Font("楷体", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.richTextBox1.Location = new System.Drawing.Point(327, 171);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(418, 330);
+            this.richTextBox1.Size = new System.Drawing.Size(813, 516);
             this.richTextBox1.TabIndex = 3;
             this.richTextBox1.Text = "";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("楷体", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(70, 44);
+            this.label1.Font = new System.Drawing.Font("楷体", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(198, 73);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(143, 40);
+            this.label1.Size = new System.Drawing.Size(211, 60);
             this.label1.TabIndex = 4;
             this.label1.Text = "label1";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::农产品物流管理系统.Properties.Resources.农户操作;
-            this.ClientSize = new System.Drawing.Size(802, 553);
+            this.ClientSize = new System.Drawing.Size(1199, 830);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button3);
